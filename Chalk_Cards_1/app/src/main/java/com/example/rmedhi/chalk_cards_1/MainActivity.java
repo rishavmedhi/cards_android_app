4768bbb8df7a68e1dc8de0cdf617b63f17c850c1
rishavmedhi <rishav@chalkstreet.com>
2017-05-06 12:41:48 +0530

    Added Butter Knife in project and made changes

--- Chalk_Cards_1/app/src/main/java/com/example/rmedhi/chalk_cards_1/MainActivity.java
+++ Chalk_Cards_1/app/src/main/java/com/example/rmedhi/chalk_cards_1/MainActivity.java
@@ -24,31 +24,32 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private static final int CAMERA_REQUEST = 1888;
     private static final int PICK_IMAGE_REQUEST = 1000;
     private Context mContext;
 
-    RelativeLayout mRelativeLayout;
     private RecyclerView mRecyclerView;
 
     private int g_pos;
-    private int preview=0;
 
-    Button mPreviewBtn;
     TextView welcometext;
 
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
     private ArrayList<CardList> cardsList = new ArrayList<>();
+
+    @BindView(R.id.rl)
+    RelativeLayout mRelativeLayout;
 
     @Override
     public MenuInflater getMenuInflater() {
@@ -87,13 +88,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
         // Get the application context
         mContext = getApplicationContext();
 
         // Get the widgets reference from XML layout
-        mRelativeLayout = (RelativeLayout) findViewById(R.id.rl);
+
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        mPreviewBtn = (Button) findViewById(R.id.preview);
         welcometext = (TextView) findViewById(R.id.welcome_text);
 
         // Define a layout for RecyclerView
@@ -109,38 +111,7 @@ public class MainActivity extends AppCompatActivity {
         // Set the adapter for RecyclerView
         mRecyclerView.setAdapter(mAdapter);
 
-        // click listener for preview button
-        mPreviewBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                // setting preview flag as 1 if preview not active
-                if (preview==0) {
-                    // if list of images is 0
-                    if (cardsList.size()==0)
-                        Toast.makeText(mContext,"Add a Card to Preview", Toast.LENGTH_SHORT).show();
-                    else {
-                        // checking if there is an image missing in a card
-                        int check_list=checklist(cardsList);
-                        if (check_list==0)
-                            Toast.makeText(mContext,"Add Image to all cards to begin preview", Toast.LENGTH_SHORT).show();
-                        else {
-                            mRecyclerView.scrollToPosition(0);
-                            preview = 1;
-                            mRelativeLayout.setBackgroundColor(Color.BLACK);
-                            Toast.makeText(mContext,"Preview Mode", Toast.LENGTH_SHORT).show();
-                            mPreviewBtn.setText("Back");
-                        }
-                    }
-                }
-                else
-                {
-                    mRelativeLayout.setBackgroundColor(Color.WHITE);
-                    preview = 0;
-                    Toast.makeText(mContext,"Editing Mode", Toast.LENGTH_SHORT).show();
-                    mPreviewBtn.setText("Preview");
-                }
-            }
-        });
+
     }
 
     public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.ViewHolder> {
@@ -154,18 +125,18 @@ public class MainActivity extends AppCompatActivity {
 
         /* view holder of the card */
         public class ViewHolder extends RecyclerView.ViewHolder{
-            public TextView mTextView;
-            public Button mGallerybtn;
-            public ImageView mImageView;
-            public RelativeLayout mRelativeLayout;
+            @BindView(R.id.tv)
+                TextView mTextView;
+            @BindView(R.id.gallery_btn)
+                Button mGallerybtn;
+            @BindView(R.id.imageView)
+                ImageView mImageView;
+            @BindView(R.id.rl)
+                RelativeLayout mRelativeLayout;
             public ViewHolder(View v){
                 super(v);
-                mTextView = (TextView) v.findViewById(R.id.tv);
-                mGallerybtn = (Button) v.findViewById(R.id.gallery_btn);
-                mRelativeLayout = (RelativeLayout) v.findViewById(R.id.rl);
-                mImageView = (ImageView) v.findViewById(R.id.imageView);
+                ButterKnife.bind(this,v);
             }
-
         }
 
         @Override
